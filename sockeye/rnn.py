fb9a6d0db61380f1a3debb4a27b5fdabfba49f92
Hieber, Felix <fhieber@amazon.de>
2017-07-11 11:45:18 +0200

    Make mypy happy

--- sockeye/rnn.py
+++ sockeye/rnn.py
@@ -11,7 +11,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from typing import Optional
+from typing import List, Optional
 
 import mxnet as mx
 
