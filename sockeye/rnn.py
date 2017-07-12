8858ceb31213aabca750ec2d80a0cff7a52aef54
David Vilar <dvilar@amazon.com>
2017-07-12 14:05:10 +0200

    Hints for unused List import in rnn.py
    Ugly

--- sockeye/rnn.py
+++ sockeye/rnn.py
@@ -11,7 +11,8 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from typing import Optional
+# List is needed for mypy, but not used in the code, only in special comments
+from typing import Optional, List  # pylint: disable=unused-import # noqa
 
 import mxnet as mx
 
