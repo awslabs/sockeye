5ff9a22a460386c8542114e9b5542d8535351420
Hieber, Felix <fhieber@amazon.de>
2017-08-06 10:23:51 -0400

    fixed doc generation

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 class MockClass(MagicMock):
 
-    def __init__(self, name, *args, **kwargs):
+    def __init__(self, name="", *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.name = name
 
