3eaca72012dc546af754ac33e36354e5eef6f46a
Michael Denkowski <mdenkows@amazon.com>
2017-09-18 12:09:41 -0400

    Address comments from PR.

--- sockeye/optimizers.py
+++ sockeye/optimizers.py
@@ -18,8 +18,9 @@ Extra optimizers not included in MXNet.
 from abc import abstractmethod
 from collections import namedtuple
 import math
+from typing import Tuple
 
-from mxnet.ndarray import NDArray, sqrt, zeros
+from mxnet.ndarray import NDArray, sqrt, zeros_like
 from mxnet.optimizer import Optimizer
 
 from sockeye.utils import check_condition
@@ -33,9 +34,9 @@ class SockeyeOptimizer(Optimizer):
     Optimizer that has access to additional information from the last batch and the last chekpoint
     when updating weights.
     """
-    def __init__(self, **kwargs):
-        self.batch_state = None
-        self.checkpoint_state = None
+    def __init__(self, **kwargs) -> None:
+        self.batch_state: BatchState = None
+        self.checkpoint_state: CheckpointState = None
         super().__init__(**kwargs)
 
     def pre_update_batch(self, batch_state: BatchState):
@@ -58,11 +59,24 @@ class SockeyeOptimizer(Optimizer):
         pass
 
 
-# convenience wrapper for Optimizer.Register
-register = Optimizer.register   # pylint: disable=invalid-name
+class EveState:
+    """
+    Storage class for Eve optimizer state information.
+    """
+    def __init__(self, weight: NDArray) -> None:
+        # Mean and variance for Adam
+        self.mean = zeros_like(weight, ctx=weight.context)
+        self.variance = zeros_like(weight, ctx=weight.context)
+        # Values for computing Eve's d term (batch)
+        self.batch_f_hat_prev = 0.
+        self.batch_d_prev = 1.
+        # Values for computing Eve's d term (checkpoint)
+        self.checkpoint_prev = 0
+        self.checkpoint_f_hat_prev = 0.
+        self.checkpoint_d_prev = 1.
 
 
-@register
+@Optimizer.register
 class Eve(SockeyeOptimizer):
     """
     The Eve optimizer is an extended version of Adam that incorporates feedback from the objective
@@ -96,9 +110,12 @@ class Eve(SockeyeOptimizer):
                  k_hi: float = 10,
                  use_batch_objective: bool = True,
                  use_checkpoint_objective: bool = False,
-                 **kwargs):
+                 **kwargs) -> None:
         check_condition(any((use_batch_objective, use_checkpoint_objective)),
                         "Must use at least one of: batch objective, checkpoint objective")
+        check_condition(kwargs.get("rescale_grad", 1.) == 1., "Eve optimizer does not support rescaling gradients.")
+        check_condition(not kwargs.get("clip_gradient", False), "Eve optimizer does not support gradient clipping.")
+        check_condition(kwargs.get("wd", 0.) == 0., "Eve optimizer does not support weight decay.")
         super().__init__(learning_rate=learning_rate, **kwargs)
         self.beta1 = beta1
         self.beta2 = beta2
@@ -110,27 +127,20 @@ class Eve(SockeyeOptimizer):
         self.use_batch_objective = use_batch_objective
         self.use_checkpoint_objective = use_checkpoint_objective
 
-    def create_state(self, index: int, weight: NDArray):
-        return (zeros(weight.shape, weight.context, dtype=weight.dtype),  # mean
-                zeros(weight.shape, weight.context, dtype=weight.dtype),  # variance
-                [0, 1],     # batch previous objective "hat", previous d
-                [0, 0, 1])  # checkpoint number, previous objective "hat", previous d
+    def create_state(self, index: int, weight: NDArray) -> EveState:
+        return EveState(weight)
 
-    def update(self,
-               index: int,
-               weight: NDArray,
-               grad: NDArray,
-               state: object):
+    def update(self, index: int, weight: NDArray, grad: NDArray, state: EveState):
 
         assert isinstance(weight, NDArray)
         assert isinstance(grad, NDArray)
         lr = self._get_lr(index)
-
-        mean, var, prev_batch, prev_checkpoint = state
-
-        # Standard Adam rules for updating mean and variance
         self._update_count(index)
         t = self._index_update_count[index]
+
+        # Standard Adam rules for updating mean and variance
+        mean = state.mean
+        var = state.variance
 
         coef1 = 1. - self.beta1**t
         coef2 = 1. - self.beta2**t
@@ -139,13 +149,10 @@ class Eve(SockeyeOptimizer):
         mean[:] = self.beta1 * mean + (1. - self.beta1) * grad
         var[:] = self.beta2 * var + (1. - self.beta2) * (grad**2)
 
-        # Now compute Eve's d term
-        d = 0.
+        # Now compute Eve's f_hat and d terms
 
-        # Eve rules from paper: compute f_hat and d based on last training batch objective
-        if self.use_batch_objective:
-            f_hat_prev, d_prev = prev_batch
-            f = self.batch_state.metric_val
+        def compute_d(t: int, f: float, f_hat_prev: float, d_prev: float, beta: float) -> Tuple[float, float]:
+            """Compute Eve's f_hat and d terms as described in paper"""
             if t > 1:
                 # The original paper has a typo in the algorithm here.  The following lines are re-
                 # written to reflect the actual logic presented in the authors' longer explanation.
@@ -159,44 +166,46 @@ class Eve(SockeyeOptimizer):
                 c = min(max(delta_lo, f / f_hat_prev), delta_hi)
                 f_hat = c * f_hat_prev
                 r = abs(f_hat - f_hat_prev) / min(f_hat, f_hat_prev)
-                d_batch = self.beta3 * d_prev + (1. - self.beta3) * r
+                d = beta * d_prev + (1. - beta) * r
             else:
                 f_hat = f
-                d_batch = 1.
-            prev_batch[:] = [f_hat, d_batch]
-            d += d_batch
+                d = 1.
+            return (f_hat, d)
 
-        # Extension: compute f_hat and d based on last validation checkpoint objective
+        # Computation occurs for each batch
+        if self.use_batch_objective:
+            batch_f_hat, batch_d = compute_d(t,
+                                             self.batch_state.metric_val,
+                                             state.batch_f_hat_prev,
+                                             state.batch_d_prev,
+                                             self.beta3)
+            state.batch_f_hat_prev = batch_f_hat
+            state.batch_d_prev = batch_d
+
         # Computation occurs once per checkpoint using the checkpoint number as t.  Prior to the
         # first checkpoint, d = 1.
         if self.use_checkpoint_objective:
-            checkpoint, f_hat_prev, d_prev = prev_checkpoint
             # Only need to recompute if we've seen a new checkpoint since the previous batch update
-            if self.checkpoint_state and self.checkpoint_state.checkpoint != checkpoint:
+            if self.checkpoint_state and self.checkpoint_state.checkpoint != state.checkpoint_prev:
                 checkpoint = self.checkpoint_state.checkpoint
-                f = self.checkpoint_state.metric_val
-                if checkpoint > 1:
-                    if f <= f_hat_prev:
-                        delta_lo = 1. / (self.k_hi + 1.)
-                        delta_hi = 1. / (self.k_lo + 1.)
-                    else:
-                        delta_lo = self.k_lo + 1.
-                        delta_hi = self.k_hi + 1.
-                    c = min(max(delta_lo, f / f_hat_prev), delta_hi)
-                    f_hat = c * f_hat_prev
-                    r = abs(f_hat - f_hat_prev) / min(f_hat, f_hat_prev)
-                    d_checkpoint = self.beta4 * d_prev + (1. - self.beta4) * r
-                else:
-                    f_hat = f
-                    d_checkpoint = 1.
-                prev_checkpoint[:] = [checkpoint, f_hat, d_checkpoint]
+                checkpoint_f_hat, checkpoint_d = compute_d(checkpoint,
+                                                           self.checkpoint_state.metric_val,
+                                                           state.checkpoint_f_hat_prev,
+                                                           state.checkpoint_d_prev,
+                                                           self.beta4)
+                state.checkpoint_prev = checkpoint
+                state.checkpoint_f_hat_prev = checkpoint_f_hat
+                state.checkpoint_d_prev = checkpoint_d
             else:
-                d_checkpoint = d_prev
-            d += d_checkpoint
+                checkpoint_d = state.checkpoint_d_prev
 
         # Batch and checkpoint contribute equally when both are used
         if self.use_batch_objective and self.use_checkpoint_objective:
-            d /= 2.
+            d = (batch_d + checkpoint_d) / 2.
+        elif self.use_batch_objective:
+            d = batch_d
+        elif self.use_checkpoint_objective:
+            d = checkpoint_d
 
         # Final weight update rule (Adam rule with extra d term)
         weight[:] = weight - lr * mean / (d * sqrt(var) + self.epsilon)
