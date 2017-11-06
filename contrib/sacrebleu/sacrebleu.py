cd4b7b025df325fbc37eb9654d4a4326f393ed6f
Matt Post <post@cs.jhu.edu>
2017-11-06 22:50:44 +0100

    headers

--- contrib/sacrebleu/sacrebleu.py
+++ contrib/sacrebleu/sacrebleu.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+
+# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not
+# use this file except in compliance with the License. A copy of the License
+# is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
 """
 SacréBLEU provides hassle-free computation of shareable, comparable, and reproducible BLEU scores.
 Inspired by Rico Sennrich's `multi-bleu-detok.perl`, it produces the official WMT scores but works with plain text.
@@ -108,6 +122,9 @@ import urllib.parse
 import argparse
 
 from collections import defaultdict, namedtuple
+
+from signal import signal, SIGPIPE, SIG_DFL
+signal(SIGPIPE,SIG_DFL) 
 
 VERSION = '1.0.3'
 
