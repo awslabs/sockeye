c97ec0176dac2a649b41ed86baf4769d7c77a7fb
Matt Post <mattpost@amazon.com>
2018-01-11 14:26:02 +0100

    now works with BPE

--- scripts/add_factor_case.py
+++ scripts/add_factor_case.py
@@ -4,6 +4,7 @@ import argparse
 import sys
 
 def case(s):
+    # print('case', s)
     if s.islower():
         return 'l'
     elif s.isupper():
@@ -13,12 +14,33 @@ def case(s):
     else:
         return '-'
 
+def eat_and_tag(tokens, i, f=case):
+    """
+    Walks forward merging BPE tokens so the word function can be computed.
+    """
+    # print('eat_and_tag', i, tokens[i:])
+    token = ''
+    while tokens[i].endswith('@@'):
+        token += tokens[i].replace('@@', '')
+        i += 1
+    token += tokens[i]
+    return i + 1, f(token)
+
 def main():
     parser = argparse.ArgumentParser()
     args = parser.parse_args()
 
     for line in sys.stdin:
-        print(' '.join([case(tok) for tok in line.rstrip().split()]))
+        tokens = line.rstrip().split()
+        tags = [] * len(tokens)
+        i = 0
+        while i < len(tokens):
+            next_i, tag = eat_and_tag(tokens, i, case)
+            tags[i:next_i] = [tag] * (next_i - i)
+#            print('tags', i, next_i, tag)
+            i = next_i
+
+        print(' '.join(tags))
 
 if __name__ == '__main__':
     main()
