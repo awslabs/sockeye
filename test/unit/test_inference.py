427ec0981e37d0e155ff053d3af4b7e866257a7a
Hieber, Felix <fhieber@amazon.de>
2018-02-06 17:52:02 +0100

    some failure tests that still fail. Need to work on the parsing

--- test/unit/test_inference.py
+++ test/unit/test_inference.py
@@ -16,6 +16,7 @@ import numpy as np
 import pytest
 
 import sockeye.inference
+from sockeye.utils import SockeyeError
 
 _BOS = 0
 _EOS = -1
@@ -114,6 +115,7 @@ def test_get_max_input_output_length(
 
 @pytest.mark.parametrize("sentence_id, raw_sentence, num_factors, delimiter, expected_tokens, expected_factors",
                          [(1, "this is a test", 0, "|", ["this", "is", "a", "test"], []),
+                          (1, "space   space", 0, "|", ["space", "space"], []),
                           (2, "", 0, "|", [], []),
                           (14, "a|l b|l C|u", 1, "|", ["a", "b", "C"], [["l", "l", "u"]]),
                           (1, "a-X-Y ", 2, "-", ["a"], [["X"], ["Y"]]),
@@ -123,10 +125,35 @@ def test_make_input(sentence_id, raw_sentence, num_factors, delimiter, expected_
                                                                raw_sentence=raw_sentence,
                                                                num_factors=num_factors,
                                                                delimiter=delimiter)
+    assert isinstance(translator_input, sockeye.inference.TranslatorInput)
     assert translator_input.sentence_id == sentence_id
     assert translator_input.chunk_id == 0
     assert translator_input.tokens == expected_tokens
     assert len(translator_input.factors) == num_factors
     assert translator_input.factors == expected_factors
+
+
+@pytest.mark.parametrize("sentence, num_factors, delimiter, expected_wrong_num_factors, expected_fail_word",
+                         [("this is a test", 1, "|", 0, "this"),
+                          ("this is", 1, " ", 0, "this"),
+                          ("this", 1, "|", 0, "this"),
+                          ("this|X is| a|", 1, "|", 1, "is"),
+                          ("this||", 1, "|", 1, "is"),
+                          ("this|| another", 1, "|", 0, "another"),
+                          ("this|||", 1, "|", 0, "this"),
+                          ("|this", 1, "|", 0, "this"),
+                          (r"this\tX is\tX", 1, r"\t", 0, "this"),
+                          ])
+def test_failed_make_input(sentence, num_factors, delimiter, expected_wrong_num_factors, expected_fail_word):
+    sentence_id = 1
+    with pytest.raises(SockeyeError) as e:
+        sockeye.inference.Translator.make_input(sentence_id=sentence_id,
+                                                raw_sentence=sentence,
+                                                num_factors=num_factors,
+                                                delimiter=delimiter)
+    assert str(e.value) == 'Expecting %d factors, but got %d at sentence %d, word "%s"' % (num_factors,
+                                                                                            expected_wrong_num_factors,
+                                                                                            sentence_id,
+                                                                                            expected_fail_word)
 
 # TODO(fhieber): failure tests and other tests for factor parsing
