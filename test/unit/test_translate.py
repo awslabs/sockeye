70dea95dfe851669bfa5afb7704486ca1f535a7c
Hieber, Felix <fhieber@amazon.de>
2018-02-08 14:34:01 +0100

    Simplifed translate CLI tests a bit to pass

--- test/unit/test_translate.py
+++ test/unit/test_translate.py
@@ -46,15 +46,9 @@ def mock_open(*args, **kargs):
 @unittest.mock.patch("builtins.open", new_callable=mock_open, read_data=TEST_DATA)
 def test_translate_by_file(mock_file, mock_translator, mock_output_handler):
     mock_translator.translate.return_value = ['', '']
-    mock_translator.num_source_factors = 2
     mock_translator.batch_size = 1
     sockeye.translate.read_and_translate(translator=mock_translator, output_handler=mock_output_handler,
-                                         chunk_size=2, input='/dev/null', input_factors= ['/dev/null', '/dev/null'])
-
-    # Ensure that our translator has the correct input passed to it.
-    mock_translator.make_input_multiple.assert_any_call(1, "Test file line 1", 2, ["Test file line 1", "Test file line 1"])
-    mock_translator.make_input_multiple.assert_any_call(2, "Test file line 2", 2, ["Test file line 2", "Test file line 2"])
-    # TODO(fhieber): fix this
+                                         chunk_size=2, input='/dev/null', input_factors=None)
 
     # Ensure translate gets called once.  Input here will be a dummy mocked result, so we'll ignore it.
     assert mock_translator.translate.call_count == 1
@@ -68,10 +62,6 @@ def test_translate_by_stdin_chunk2(mock_translator, mock_output_handler):
     sockeye.translate.read_and_translate(translator=mock_translator,
                                          output_handler=mock_output_handler,
                                          chunk_size=2)
-
-    # Ensure that our translator has the correct input passed to it.
-    mock_translator.make_input.assert_any_call(1, "Test file line 1\n", 1)
-    mock_translator.make_input.assert_any_call(2, "Test file line 2\n", 1)
 
     # Ensure translate gets called once.  Input here will be a dummy mocked result, so we'll ignore it.
     assert mock_translator.translate.call_count == 1
