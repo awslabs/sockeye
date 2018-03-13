ad990095a2fe84dbf84b72b886a4c7f66d31f648
Hieber, Felix <fhieber@amazon.de>
2018-03-13 08:35:05 +0100

    Fixed t2t train/test scripts

--- arxiv/code/transformer/tensor2tensor/test-transformer.sh
+++ arxiv/code/transformer/tensor2tensor/test-transformer.sh
@@ -2,17 +2,28 @@
 
 . env.sh
 
-INPUT=$DATADIR/$PAIR/test.bpe.$SOURCE
-OUTPUT=$PAIR.output
-PYTHONPATH=$SOCKEYE python3 -m sockeye.translate \
-    -m $PAIR -c 250 \
-    --batch-size 16 --chunk-size 1000 \
-    -i $INPUT -o $OUTPUT \
-    --beam-size 5 --length-penalty-alpha 1.0 \
-    2> $OUTPUT.log
+USRDIR=t2t_usr
+DATADIR=data_$PAIR
+TMPDIR=tmp_$PAIR
+PROBLEM=wmt17_${SOURCE}_${TARGET}_bpe32k
+HPARAMS=transformer_wmt17_base
+MODEL=transformer.$PROBLEM.$HPARAMS
 
-cat $OUTPUT | \
+BEAM=5
+ALPHA=1.0
+INPUTS=$DATDIR/$PAIR/test.bpe.$SOURCE
+OUTPUTS=$(basename $INPUTS).transformer.$HPARAMS.$PROBLEM.beam$BEAM.alpha$ALPHA.decodes
+
+t2t-decoder --t2t_usr_dir=$USRDIR --data_dir=$DATADIR --tmp_dir=$TMPDIR --problems=$PROBLEM \
+    --model=transformer \
+    --hparams_set=$HPARAMS \
+    --output_dir=$OUTPUT \
+    --decode_hparams="beam_size=$BEAM,alpha=$ALPHA,batch_size=16,use_last_position_only=True" \
+    --decode_from_file=$INPUTS \
+    --decode_to_file=$OUTPUTS
+
+cat $OUTPUTS | \
     sed 's/\@\@ //g' | \
     $MOSES/scripts/tokenizer/detokenizer.perl -q -l $TARGET | \
-    tee $OUTPUT.detok | \
+    tee $OUTPUTS.detok | \
     sacrebleu -t wmt17 -l $PAIR > $OUTPUT.bleu
--- arxiv/code/transformer/tensor2tensor/train-transformer.sh
+++ arxiv/code/transformer/tensor2tensor/train-transformer.sh
@@ -9,21 +9,15 @@ PROBLEM=wmt17_${SOURCE}_${TARGET}_bpe32k
 HPARAMS=transformer_wmt17_base
 MODEL=transformer.$PROBLEM.$HPARAMS
 
-BEAM=5
-ALPHA=1.0
 INPUTS=$DATDIR/$PAIR/test.bpe.$SOURCE
 OUTPUTS=$(basename $INPUTS).transformer.$HPARAMS.$PROBLEM.beam$BEAM.alpha$ALPHA.decodes
 
-t2t-decoder --t2t_usr_dir=$USRDIR --data_dir=$DATADIR --tmp_dir=$TMPDIR --problems=$PROBLEM \
+t2t-trainer --t2t_usr_dir=$USRDIR --data_dir=$DATADIR --tmp_dir=$TMPDIR --problems=$PROBLEM \
     --model=transformer \
     --hparams_set=$HPARAMS \
-    --output_dir=$OUTPUT \
-    --decode_hparams="beam_size=$BEAM,alpha=$ALPHA,batch_size=16,use_last_position_only=True" \
-    --decode_from_file=$INPUTS \
-    --decode_to_file=$OUTPUTS
-
-cat $OUTPUTS | \
-    sed 's/\@\@ //g' | \
-    $MOSES/scripts/tokenizer/detokenizer.perl -q -l $TARGET | \
-    tee $OUTPUTS.detok | \
-    sacrebleu -t wmt17 -l $PAIR > $OUTPUT.bleu
+    --train_steps=1000000 \
+    --save_checkpoints_secs=1200 \
+    --local_eval_frequency=0 \
+    --schedule=train \
+    --worker_gpu 2 \
+    --output_dir=$MODEL 2> $MODEL.log
