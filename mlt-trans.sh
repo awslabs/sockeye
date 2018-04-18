96757a7a37c0a46126b4df25a4f008bd9d5c8c38
rongzha1 <rong.a.zhang@intel.com>
2018-04-18 14:22:17 +0800

    add chapter multi-instance translate for README.md and script mlt-trans to run multi-instance

---
+++ mlt-trans.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+# Describtion: This scipt is used for CPU  Multi-instance Translate, 
+#              which can  greatly speedup translate performance.
+# FileName: mlt-trans.sh
+# Usage: ./mlt-trans.sh model_name file_to_translate batch_size
+# Version: 1.0
+
+
+if [ "$#" -eq "3" ];then
+	model=$1
+	filetotrans=$2
+	bs=$3
+	if [ ! -d $1 ];then
+		echo "$1 model does not exist!"
+		exit
+	fi
+	if [ ! -f $2 ];then
+		echo "$2 translate file does not exist"
+		exit
+	fi
+
+else
+	echo 'Run error!'
+	echo 'Usage: ./mlt-trans.sh model_name file_to_translate batch_size'
+	echo 'Example: ./mlt-trans.sh wmt_model newstest2016.tc.BPE.de 32'
+	exit
+fi
+
+export KMP_AFFINITY=granularity=fine,noduplicates,compact,1,0
+
+vCPU=`cat /proc/cpuinfo |grep "processor"|wc -l`
+cores=`expr $vCPU / 2`
+
+loop=$cores
+stride=$(($cores/$loop))
+export OMP_NUM_THREADS=$stride
+
+echo 'Translating...'
+start=$(($(date +%s%N)/1000000000))	
+for ((i=0; i<$loop; i++))
+	do
+		startCore=$[${i}*${stride}]
+		endCore=$[${startCore}+${stride}-1]
+    
+		if [ $i == $[$loop-1] ]
+		then
+ 			taskset -c ${startCore}-${endCore} python3 -m sockeye.translate -m $model -i $filetotrans -o newstest2016.tc.BPE.en --batch-size $bs --output-type benchmark --use-cpu > /dev/null 2>&1 
+		else
+			taskset -c ${startCore}-${endCore} python3 -m sockeye.translate -m $model -i $filetotrans -o newstest2016.tc.BPE.en --batch-size $bs --output-type benchmark --use-cpu  > /dev/null 2>&1 & 
+		fi
+	done
+end=$(($(date +%s%N)/1000000000))
+
+diff=$(($end-$start))
+sentences=`cat newstest2016.tc.BPE.de | wc -l`
+speed=`echo "scale=3;$sentences*$loop/$diff"|bc`
+
+echo "Instance nums : " $cores
+echo "Total Processed lines : " $[$sentences*$cores]
+echo "Total time(s) : " `echo "scale=3;$diff"|bc`
+echo "speed : " $speed "sent/sec"
+
