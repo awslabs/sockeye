c5e586e3dd6dd6399f8052dd5c3ea2f52cd945e4
Hieber, Felix <fhieber@amazon.de>
2018-06-21 17:41:23 +0200

    Hybridize with static_alloc/shape

--- sockeye/inference.py
+++ sockeye/inference.py
@@ -1002,7 +1002,7 @@ class Translator:
 
         self._update_scores = UpdateScores()
         self._update_scores.initialize(ctx=self.context)
-        self._update_scores.hybridize()
+        self._update_scores.hybridize(static_alloc=True, static_shape=True)
 
         # topk function used in beam search
         # self.topk = partial(utils.topk,
@@ -1013,26 +1013,26 @@ class Translator:
 
         self._topk = TopK(k=self.beam_size, batch_size=self.batch_size, vocab_size=len(self.vocab_target))
         self._topk.initialize(ctx=self.context)
-        self._topk.hybridize()
+        self._topk.hybridize(static_alloc=True, static_shape=True)
 
         self._sort_by_index = SortByIndex()
         self._sort_by_index.initialize(ctx=self.context)
-        self._sort_by_index.hybridize()
+        self._sort_by_index.hybridize(static_alloc=True, static_shape=True)
 
         self._normalize_finished = NormalizeFinishedHypotheses(pad_id=C.PAD_ID,
                                                                eos_id=self.vocab_target[C.EOS_SYMBOL],
                                                                length_penalty_alpha=self.length_penalty.alpha,
                                                                length_penalty_beta=self.length_penalty.beta)
         self._normalize_finished.initialize(ctx=self.context)
-        self._normalize_finished.hybridize()
+        self._normalize_finished.hybridize(static_alloc=True, static_shape=True)
         self._prune_hyps = PruneHypotheses(threshold=self.beam_prune, beam_size=self.beam_size)
         self._prune_hyps.initialize(ctx=self.context)
-        self._prune_hyps.hybridize()
+        self._prune_hyps.hybridize(static_alloc=True, static_shape=True)
 
         self._update_lengths_and_finished = UpdateLengthsAndFinished(pad_id=C.PAD_ID,
                                                                      eos_id=self.vocab_target[C.EOS_SYMBOL])
         self._update_lengths_and_finished.initialize(ctx=self.context)
-        self._update_lengths_and_finished.hybridize()
+        self._update_lengths_and_finished.hybridize(static_alloc=True, static_shape=True)
 
         logger.info("Translator (%d model(s) beam_size=%d beam_prune=%s beam_search_stop=%s "
                     "ensemble_mode=%s batch_size=%d buckets_source=%s)",
