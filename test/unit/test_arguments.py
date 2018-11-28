a032e867fccd3e812701cd7e805a54ec7536b69d
Matt Post <post@cs.jhu.edu>
2018-11-28 14:58:05 -0500

    fixed test case

--- test/unit/test_arguments.py
+++ test/unit/test_arguments.py
@@ -219,7 +219,7 @@ def test_training_arg(test_params, expected_params):
                       length_penalty_beta=0.0,
                       strip_unknown_words=False,
                       override_dtype=None,
-                      sample=False,
+                      sample=None,
                       seed=C.DEFAULT_RANDOM_SEED,
                       skip_topk=False)),
 ])
