486c5965b6683da7e7bca76fc859169d58c5466b
Hieber, Felix <fhieber@amazon.de>
2019-06-07 17:37:07 +0200

    Fix test_config

--- test/unit/test_config.py
+++ test/unit/test_config.py
@@ -28,43 +28,12 @@ class ConfigTest(config.Config):
         self.config = config
 
 
-def test_base_freeze():
-    c = config.Config()
-    c.param = 1
-    assert c.param == 1
-    c.freeze()
-    with pytest.raises(AttributeError) as e:
-        c.param = 2
-    assert str(e.value) == "Cannot set 'param' in frozen config"
-
-
-def test_freeze():
-    c1 = ConfigTest(param=1)
-    c2 = ConfigTest(param=3)
-    c1.param = 2
-    assert c1.param == 2
-    c1.config = c2
-    assert c2 == c1.config
-    c1.config.param = 2
-    assert c1.config.param == 2
-    c1.freeze()
-    assert c1.config._frozen  # pylint: disable= no-member
-    assert c2._frozen  # pylint: disable= no-member
-    with pytest.raises(AttributeError) as e:
-        c1.param = 3
-    assert str(e.value) == "Cannot set 'param' in frozen config"
-    with pytest.raises(AttributeError) as e:
-        c1.config.param = 3
-    assert str(e.value) == "Cannot set 'param' in frozen config"
-
-
 def test_config_repr():
     c1 = ConfigTest(param=1, config=ConfigTest(param=3))
-    c1.config.freeze()
-    assert str(c1) == "Config[_frozen=False, config=Config[_frozen=True, config=None, param=3], param=1]"
+    assert str(c1) == "Config[config=Config[config=None, param=3], param=1]"
 
 
-def test_eq():
+def test_config_eq():
     basic_c = config.Config()
     c1 = ConfigTest(param=1)
     c1_other = ConfigTest(param=1)
@@ -82,14 +51,14 @@ def test_eq():
     assert c_nested != c_nested_c2
 
 
-def test_no_self_attribute():
+def test_config_no_self_attribute():
     c1 = ConfigTest(param=1)
     with pytest.raises(AttributeError) as e:
         c1.config = c1
     assert str(e.value) == "Cannot set self as attribute"
 
 
-def test_serialization():
+def test_config_serialization():
     c1 = ConfigTest(param=1, config=ConfigTest(param=2))
     expected_serialization = """!ConfigTest
 config: !ConfigTest
@@ -99,7 +68,6 @@ param: 1
 """
     with tempfile.TemporaryDirectory() as tmp_dir:
         fname = os.path.join(tmp_dir, "config")
-        c1.freeze()
         c1.save(fname)
         assert os.path.exists(fname)
         with open(fname) as f:
@@ -108,10 +76,9 @@ param: 1
         c2 = config.Config.load(fname)
         assert c2.param == c1.param
         assert c2.config.param == c1.config.param
-        assert not c2._frozen
 
 
-def test_copy():
+def test_config_copy():
     c1 = ConfigTest(param=1)
     copy_c1 = c1.copy()
     # should be a different object that is equal to the original object
@@ -133,7 +100,7 @@ class ConfigWithMissingAttributes(config.Config):
         self.new_attribute = new_attribute
 
 
-def test_missing_attributes_filled_with_default():
+def test_config_missing_attributes_filled_with_default():
     # when we load a configuration object that does not contain all attributes as the current version of the
     # configuration object we expect the missing attributes to be filled with the default values taken from the
     # __init__ method.
