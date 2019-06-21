7c969696966c3fefe9fe642a2a30ccd89c485b29
Ubuntu <herbrich@workhorse.vygrx3nu1irurk5mbddaiynp1f.bx.internal.cloudapp.net>
2019-06-21 14:33:56 +0000

    First working version

--- reverse/gen_reverse_dataset.py
+++ reverse/gen_reverse_dataset.py
@@ -16,45 +16,62 @@ CLI for generating datasets for testing reverse sequence copying
 """
 
 import argparse
-# import os
-# import random
+import os
+import random
 
-def main():
+def parse_arguments():
+    """
+    Parses the command line arguments.
+    
+    :return: An object containing all arguments are fields.
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--num_samples", help="the total number of samples")
+    parser.add_argument("-nt", "--num-train", type=int, default=10000, help="the total number of training samples")
+    parser.add_argument("-nv", "--num-val", type=int, default=100, help="the total number of validaton samples")
+    parser.add_argument("-sm", "--min-sequence-length", type=int, default=5, help="the minimum length of sequence")
+    parser.add_argument("-sx", "--max-sequence-length", type=int, default=15, help="the maximum length of sequence")
+    parser.add_argument("-s", "--seed", type=int, default=42, help="the random seed for the data generation")
+    parser.add_argument("output", default="data", help="the name of the output directory")
     args = parser.parse_args()
 
-    print(args.num_samples)
+    print("Number of training samples: ", args.num_train)
+    print("Number of validation samples: ", args.num_val)
+    print("Minimum length of sequences: ", args.min_sequence_length)
+    print("Number of training samples: ", args.max_sequence_length)
+    print("Random seed: ", args.seed)
+    print("Output directory: ", args.output)
+    return(args)
+
+def main():
+    """
+    Main entry point to the app. 
+    """
+
+    args = parse_arguments()
+    random.seed(args.seed)
+
+    samples = set()
+    for i in range(0, args.num_train+args.num_val):
+        seq_len = random.randint(args.min_sequence_length, args.max_sequence_length)
+        samples.add(" ".join(str(random.randint(0, 9)) for j in range(0, seq_len)))
+
+    samples = list(samples)
+
+    train_samples = samples[1:args.num_train]
+    val_samples = samples[(args.num_train+1):]
+
+    if not os.path.exists(args.output):
+        os.mkdir(args.output)
+    with open(args.output+"/train.source", "w") as source, open(args.output+"/train.target", "w") as target:
+        for sample in train_samples:
+            source.write(sample + "\n")
+            target.write(sample[::-1] + "\n")
+
+    with open(args.output+"/validation.source", "w") as source, open(args.output+"/validation.target", "w") as target:
+        for sample in val_samples:
+            source.write(sample + "\n")
+            target.write(sample[::-1] + "\n")
 
 main()
 
-# random.seed(12)
-
-# num_samples = 100000
-# num_dev = 1000
-# min_seq_len = 10
-# max_seq_len = 30
-# vocab_size = 10
-
-# samples = set()
-# for i in range(0, num_samples):
-#     seq_len = random.randint(min_seq_len, max_seq_len)
-#     samples.add(" ".join(str(random.randint(0, vocab_size)) for j in range(0, seq_len)))
-
-# samples = list(samples)
-
-# train_samples = samples[:num_samples-num_dev]
-# dev_samples = samples[num_samples-num_dev:]
-
-# if not os.path.exists('data'):
-#     os.mkdir('data')
-# with open("data/train.source", "w") as source, open("data/train.target", "w") as target:
-#     for sample in train_samples:
-#         source.write(sample + "\n")
-#         target.write(sample + "\n")
-
-# with open("data/dev.source", "w") as source, open("data/dev.target", "w") as target:
-#     for sample in dev_samples:
-#         source.write(sample + "\n")
-#         target.write(sample + "\n")
 
