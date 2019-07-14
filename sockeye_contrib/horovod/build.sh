24124b4f47dfe754dada3e1ce187e77cc27e2837
Michael Denkowski <mdenkows@amazon.com>
2019-07-14 10:45:56 -0500

    Tag Docker image with commit

--- sockeye_contrib/horovod/build.sh
+++ sockeye_contrib/horovod/build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 DOCKERFILE="sockeye_contrib/horovod/Dockerfile"
-REPO="sockeye"
-TAG="2-horovod"
 
 if [ ! -f ${DOCKERFILE} ]; then
   echo "This script should be run from the sockeye root directory (containing ${DOCKERFILE})."
   exit 2
 fi
 
+REPO="sockeye-horovod"
+TAG=$(git rev-parse --short HEAD)
 SOCKEYE_COMMIT=$(git rev-parse HEAD)
 
 docker build -t ${REPO}:${TAG} -f $DOCKERFILE . --build-arg SOCKEYE_COMMIT=${SOCKEYE_COMMIT}
