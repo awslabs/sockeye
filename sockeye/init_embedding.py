248ca882779f320107001a1790dedce379dcd30f
Hieber, Felix <fhieber@amazon.de>
2019-08-27 10:25:46 +0200

    Update license headers for 2019

--- sockeye/init_embedding.py
+++ sockeye/init_embedding.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2017--2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You may not
 # use this file except in compliance with the License. A copy of the License
