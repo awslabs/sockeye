b47f9a9a660e36c8e976d9c5a4a3fa18090d071c
Kenneth Heafield <kheafiel@amazon.com>
2019-12-20 10:23:36 +0000

    Copyright on quantization.py, spacing fix

--- sockeye/quantization.py
+++ sockeye/quantization.py
@@ -1,4 +1,4 @@
-# Copyright 2017--2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You may not
 # use this file except in compliance with the License. A copy of the License
@@ -13,6 +13,23 @@
 
 import mxnet as mx
 
+# Modified from the source to mxnet.gluon.nn.basic_layers.Dense which is:
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 class QuantizableDense(mx.gluon.HybridBlock):
     r"""Optionally Quantized fully-connected NN layer.
 
@@ -104,7 +121,7 @@ class QuantizableDense(mx.gluon.HybridBlock):
             super(QuantizableDense, self).cast(dtype)
         else:
             #No casting an already quantized matrix.
-              logger.warning("Ignoring casting on int8 matrix")
+            logger.warning("Ignoring casting on int8 matrix")
 
     def hybrid_forward(self, F, x, weight, scaling = None, bias=None):
         if self._dtype == 'int8':
