awslabs/sockeye

commit c00da5257c7a1958a71d3ae3079bd8de808276cf

Distributed training update (#827)

sockeye/__init__.py
@@ -11,4 +11,4 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-__version__ = '2.1.11'
+__version__ = '2.1.12'

sockeye/data_io.py
@@ -149,27 +149,33 @@ class BucketBatchSize:
 
 def define_bucket_batch_sizes(buckets: List[Tuple[int, int]],
                               batch_size: int,
-                              batch_by_words: bool,
+                              batch_type: str,
                               batch_num_devices: int,
                               data_target_average_len: List[Optional[float]],
                               batch_sentences_multiple_of: int = 1) -> List[BucketBatchSize]:
     """
-    Computes bucket-specific batch sizes (sentences, average_words).
+    Compute bucket-specific batch sizes (sentences, average_target_words).
 
-    If sentence-based batching: number of sentences is the same for each batch, determines the
-    number of words. Hence all batch sizes for each bucket are equal.
+    If sentence batching: number of sentences is the same for each batch.
 
-    If word-based batching: number of sentences for each batch is set to the multiple of number
-    of devices that produces the number of words closest to the target batch size.  Average
-    target sentence length (non-padding symbols) is used for word number calculations.
+    If word batching: number of sentences for each batch is the multiple of
+    number of devices that produces the number of words closest to the target
+    batch size. Number of sentences is finally rounded to the nearest multiple
+    of batch_sentences_multiple_of * batch_num_devices. Average target sentence
+    length (non-padding symbols) is used for word number calculations.
+
+    If max-word batching: number of sentences for each batch is set to the
+    multiple of batch_sentences_multiple_of * batch_num_devices that is closest
+    to batch_size without exceeding the value.
 
     :param buckets: Bucket list.
     :param batch_size: Batch size.
-    :param batch_by_words: Batch by words.
+    :param batch_type: Type of batching.
     :param batch_num_devices: Number of devices.
-    :param data_target_average_len: Optional average target length for each bucket.
-    :param batch_sentences_multiple_of: Round the number of sentences in each
-        bucket's batch to a multiple of this value (word-based batching only).
+    :param data_target_average_len: Optional average target length for each
+        bucket.
+    :param batch_sentences_multiple_of: Guarantee the number of sentences in
+        each bucket's batch to a multiple of this value.
     """
     check_condition(len(data_target_average_len) == len(buckets),
                     "Must provide None or average target length for each bucket")
@@ -187,26 +193,46 @@ def define_bucket_batch_sizes(buckets: List[Tuple[int, int]],
             data_target_average_len[buck_idx] = padded_seq_len
         average_seq_len = data_target_average_len[buck_idx]
 
-        # Word-based: num words determines num sentences
-        # Sentence-based: num sentences determines num words
-        if batch_by_words:
+        # Batch size for each bucket is measured in sentences:
+        # - word batching: convert average word-based size to number of
+        #       sequences
+        # - max-word batching: convert max word-based size to number of
+        #       sequences
+        # - sentence batching: use batch size directly
+        if batch_type == C.BATCH_TYPE_WORD:
             check_condition(padded_seq_len <= batch_size, "Word batch size must cover sequence lengths for all"
                                                           " buckets: (%d > %d)" % (padded_seq_len, batch_size))
             # Multiple of minimum batch step closest to target number of words,
             # assuming each sentence is of average length
             batch_size_seq = min_batch_step * max(1, round((batch_size / average_seq_len) / min_batch_step))
-            batch_size_word = batch_size_seq * average_seq_len
-        else:
+        elif batch_type == C.BATCH_TYPE_MAX_WORD:
+            check_condition(padded_seq_len <= batch_size,
+                            'Word batch size must cover sequence lengths for all buckets: (%d > %d)'
+                            % (padded_seq_len, batch_size))
+            # Max number of sequences without exceeding batch size
+            batch_size_seq = batch_size // padded_seq_len
+            # Round down to closest multiple
+            batch_size_seq = (batch_size_seq // batch_sentences_multiple_of) * batch_sentences_multiple_of
+        elif batch_type == C.BATCH_TYPE_SENTENCE:
             batch_size_seq = batch_size
-            batch_size_word = batch_size_seq * average_seq_len
+        else:
+            raise ValueError('Unknown batch type: %s' % batch_type)
+        # Batch size is per device
+        batch_size_seq *= batch_num_devices
+        # Number of words here is an average of non-padding tokens
+        batch_size_word = batch_size_seq * average_seq_len
+
         bucket_batch_sizes.append(BucketBatchSize(bucket, batch_size_seq, batch_size_word))
         # Track largest number of source or target word samples in a batch
         largest_total_num_words = max(largest_total_num_words, batch_size_seq * max(*bucket))
 
-    # Final step: guarantee that largest bucket by sequence length also has a batch size so that it covers any
-    # (batch_size, len_source) and (batch_size, len_target) matrix from the data iterator to allow for memory sharing.
-    # When batching by sentences, this will already be the case.
-    if batch_by_words:
+    # TODO: This is a legacy step from the bucketing module version of Sockeye.
+    #       It is no longer necessary but is preserved to keep training behavior
+    #       consistent.  Determine whether this can be safely removed.
+    # Final step for average word-based batching: guarantee that largest bucket
+    # by sequence length also has a shape that covers any (batch_size,
+    # len_source) and (batch_size, len_target).
+    if batch_type == C.BATCH_TYPE_WORD:
         padded_seq_len = max(*buckets[-1])
         average_seq_len = data_target_average_len[-1]
         while bucket_batch_sizes[-1].batch_size * padded_seq_len < largest_total_num_words:
@@ -214,6 +240,7 @@ def define_bucket_batch_sizes(buckets: List[Tuple[int, int]],
                 bucket_batch_sizes[-1].bucket,
                 bucket_batch_sizes[-1].batch_size + min_batch_step,
                 bucket_batch_sizes[-1].average_target_words_per_batch + min_batch_step * average_seq_len)
+
     return bucket_batch_sizes
 
 
@@ -538,7 +565,7 @@ def get_num_shards(num_samples: int, samples_per_shard: int, min_num_shards: int
 
 
 def save_shard(shard_idx: int, data_loader: RawParallelDatasetLoader,
-               shard_sources: List[str], shard_target: str, 
+               shard_sources: List[str], shard_target: str,
                shard_stats: 'DataStatistics', output_prefix: str, keep_tmp_shard_files: bool):
     """
     Load shard source and target data files into NDArrays and save to disk.
@@ -550,7 +577,7 @@ def save_shard(shard_idx: int, data_loader: RawParallelDatasetLoader,
     :param shard_target: A target file name.
     :param shard_stats: The statistics for the sources/target data.
     :param output_prefix: The prefix of the output file name.
-    :param keep_tmp_shard_files: Keep the sources/target files when it is True otherwise delete them. 
+    :param keep_tmp_shard_files: Keep the sources/target files when it is True otherwise delete them.
     """
     sources_sentences = [SequenceReader(s) for s in shard_sources]
     target_sentences = SequenceReader(shard_target)
@@ -635,7 +662,7 @@ def prepare_data(source_fnames: List[str],
         results = []
         pool = multiprocessing.pool.Pool(processes=max_processes)
         for shard_idx, (shard_sources, shard_target, shard_stats) in enumerate(shards):
-            args = (shard_idx, data_loader, shard_sources, shard_target, 
+            args = (shard_idx, data_loader, shard_sources, shard_target,
                     shard_stats, output_prefix, keep_tmp_shard_files)
             result = pool.apply_async(save_shard, args=args)
             results.append(result)
@@ -749,7 +776,7 @@ def get_prepared_data_iters(prepared_data_dir: str,
                             validation_target: str,
                             shared_vocab: bool,
                             batch_size: int,
-                            batch_by_words: bool,
+                            batch_type: str,
                             batch_num_devices: int,
                             batch_sentences_multiple_of: int = 1,
                             permute: bool = True) -> Tuple['BaseParallelSampleIter',
@@ -798,7 +825,7 @@ def get_prepared_data_iters(prepared_data_dir: str,
 
     bucket_batch_sizes = define_bucket_batch_sizes(buckets,
                                                    batch_size,
-                                                   batch_by_words,
+                                                   batch_type,
                                                    batch_num_devices,
                                                    config_data.data_statistics.average_len_target_per_bucket,
                                                    batch_sentences_multiple_of)
@@ -840,7 +867,7 @@ def get_training_data_iters(sources: List[str],
                             target_vocab_path: Optional[str],
                             shared_vocab: bool,
                             batch_size: int,
-                            batch_by_words: bool,
+                            batch_type: str,
                             batch_num_devices: int,
                             max_seq_len_source: int,
                             max_seq_len_target: int,
@@ -864,7 +891,7 @@ def get_training_data_iters(sources: List[str],
     :param target_vocab_path: Path to target vocabulary.
     :param shared_vocab: Whether the vocabularies are shared.
     :param batch_size: Batch size.
-    :param batch_by_words: Size batches by words rather than sentences.
+    :param batch_type: Method for sizing batches.
     :param batch_num_devices: Number of devices batches will be parallelized across.
     :param max_seq_len_source: Maximum source sequence length.
     :param max_seq_len_target: Maximum target sequence length.
@@ -903,7 +930,7 @@ def get_training_data_iters(sources: List[str],
 
     bucket_batch_sizes = define_bucket_batch_sizes(buckets,
                                                    batch_size,
-                                                   batch_by_words,
+                                                   batch_type,
                                                    batch_num_devices,
                                                    data_statistics.average_len_target_per_bucket,
                                                    batch_sentences_multiple_of)
@@ -1750,7 +1777,7 @@ class ShardedParallelSampleIter(BaseParallelSampleIter):
 
             if horovod_mpi.using_horovod():
                 # Synchronize shard order across workers
-                horovod_mpi.MPI.COMM_WORLD.bcast(self.shards_fnames, root=0)
+                self.shards_fnames = horovod_mpi.MPI.COMM_WORLD.bcast(self.shards_fnames, root=0)
 
             self.shard_index = 0
             self._load_shard()

test/unit/test_data_io.py
@@ -214,13 +214,13 @@ def test_parallel_iter(source_iterables, target_iterable, expected):
 
 
 def test_sample_based_define_bucket_batch_sizes():
-    batch_by_words = False
+    batch_type = C.BATCH_TYPE_SENTENCE
     batch_size = 32
     max_seq_len = 100
     buckets = data_io.define_parallel_buckets(max_seq_len, max_seq_len, 10, 1, 1.5)
     bucket_batch_sizes = data_io.define_bucket_batch_sizes(buckets=buckets,
                                                            batch_size=batch_size,
-                                                           batch_by_words=batch_by_words,
+                                                           batch_type=batch_type,
                                                            batch_num_devices=1,
                                                            data_target_average_len=[None] * len(buckets))
     for bbs in bucket_batch_sizes:
@@ -237,14 +237,14 @@ def test_sample_based_define_bucket_batch_sizes():
         (1.5, 1, [100.0, 50.0, 33.0, 25.0, 20.0, 20.0, 20.0, 20.0]),
         (1.5, 8, [96.0, 48.0, 32.0, 24.0, 16.0, 16.0, 16.0, 24.0])])
 def test_word_based_define_bucket_batch_sizes(length_ratio, batch_sentences_multiple_of, expected_batch_sizes):
-    batch_by_words = True
+    batch_type = C.BATCH_TYPE_WORD
     batch_num_devices = 1
     batch_size = 1000
     max_seq_len = 50
-    buckets = data_io.define_parallel_buckets(max_seq_len, max_seq_len, 10, 1, length_ratio)
+    buckets = data_io.define_parallel_buckets(max_seq_len, max_seq_len, 10, True, length_ratio)
     bucket_batch_sizes = data_io.define_bucket_batch_sizes(buckets=buckets,
                                                            batch_size=batch_size,
-                                                           batch_by_words=batch_by_words,
+                                                           batch_type=batch_type,
                                                            batch_num_devices=batch_num_devices,
                                                            data_target_average_len=[None] * len(buckets),
                                                            batch_sentences_multiple_of=batch_sentences_multiple_of)
@@ -261,6 +261,29 @@ def test_word_based_define_bucket_batch_sizes(length_ratio, batch_sentences_mult
     assert last_bbs.batch_size >= min_expected_batch_size
     last_bbs_num_words = last_bbs.batch_size * max(*last_bbs.bucket)
     assert last_bbs_num_words >= max_num_words
+
+
+@pytest.mark.parametrize("length_ratio,batch_sentences_multiple_of,expected_batch_sizes", [
+        # Reference batch sizes manually inspected for sanity.
+        (0.5, 1, [200, 100, 66, 50, 40, 33, 28, 25, 22, 20]),
+        (1.5, 1, [100, 50, 33, 25, 20, 20, 20, 20]),
+        (1.5, 8, [96, 48, 32, 24, 16, 16, 16, 16])])
+def test_max_word_based_define_bucket_batch_sizes(length_ratio, batch_sentences_multiple_of, expected_batch_sizes):
+    batch_type = C.BATCH_TYPE_MAX_WORD
+    batch_num_devices = 1
+    batch_size = 1000
+    max_seq_len = 50
+    buckets = data_io.define_parallel_buckets(max_seq_len, max_seq_len, 10, True, length_ratio)
+    bucket_batch_sizes = data_io.define_bucket_batch_sizes(buckets=buckets,
+                                                           batch_size=batch_size,
+                                                           batch_type=batch_type,
+                                                           batch_num_devices=batch_num_devices,
+                                                           data_target_average_len=[None] * len(buckets),
+                                                           batch_sentences_multiple_of=batch_sentences_multiple_of)
+    for bbs, expected_batch_size in zip(bucket_batch_sizes, expected_batch_sizes):
+        assert bbs.batch_size == expected_batch_size
+        expected_average_target_words_per_batch = expected_batch_size * bbs.bucket[1]
+        assert bbs.average_target_words_per_batch == expected_average_target_words_per_batch
 
 
 def _get_random_bucketed_data(buckets: List[Tuple[int, int]],
@@ -311,7 +334,7 @@ def test_parallel_data_set_fill_up():
     buckets = data_io.define_parallel_buckets(100, 100, 10, 1, 1.0)
     bucket_batch_sizes = data_io.define_bucket_batch_sizes(buckets,
                                                            batch_size,
-                                                           batch_by_words=False,
+                                                           batch_type=C.BATCH_TYPE_SENTENCE,
                                                            batch_num_devices=1,
                                                            data_target_average_len=[None] * len(buckets))
     dataset = data_io.ParallelDataSet(*_get_random_bucketed_data(buckets, min_count=1, max_count=5))
@@ -352,7 +375,7 @@ def test_parallel_data_set_permute():
     buckets = data_io.define_parallel_buckets(100, 100, 10, 1, 1.0)
     bucket_batch_sizes = data_io.define_bucket_batch_sizes(buckets,
                                                            batch_size,
-                                                           batch_by_words=False,
+                                                           batch_type=C.BATCH_TYPE_SENTENCE,
                                                            batch_num_devices=1,
                                                            data_target_average_len=[None] * len(buckets))
     dataset = data_io.ParallelDataSet(*_get_random_bucketed_data(buckets, min_count=0, max_count=5)).fill_up(
@@ -379,7 +402,7 @@ def test_get_batch_indices():
     buckets = data_io.define_parallel_buckets(100, 100, 10, 1, 1.0)
     bucket_batch_sizes = data_io.define_bucket_batch_sizes(buckets,
                                                            batch_size,
-                                                           batch_by_words=False,
+                                                           batch_type=C.BATCH_TYPE_SENTENCE,
                                                            batch_num_devices=1,
                                                            data_target_average_len=[None] * len(buckets))
     dataset = data_io.ParallelDataSet(*_get_random_bucketed_data(buckets=buckets,
@@ -484,7 +507,7 @@ def test_get_training_data_iters():
             target_vocab_path=None,
             shared_vocab=True,
             batch_size=batch_size,
-            batch_by_words=False,
+            batch_type=C.BATCH_TYPE_SENTENCE,
             batch_num_devices=1,
             max_seq_len_source=train_max_length,
             max_seq_len_target=train_max_length,
@@ -551,7 +574,7 @@ def test_parallel_sample_iter():
     bucket_counts = [0] + [None] * (len(buckets) - 1)
     bucket_batch_sizes = data_io.define_bucket_batch_sizes(buckets,
                                                            batch_size,
-                                                           batch_by_words=False,
+                                                           batch_type=C.BATCH_TYPE_SENTENCE,
                                                            batch_num_devices=1,
                                                            data_target_average_len=[None] * len(buckets))
 
@@ -609,7 +632,7 @@ def test_sharded_parallel_sample_iter():
     bucket_counts = [0] + [None] * (len(buckets) - 1)
     bucket_batch_sizes = data_io.define_bucket_batch_sizes(buckets,
                                                            batch_size,
-                                                           batch_by_words=False,
+                                                           batch_type=C.BATCH_TYPE_SENTENCE,
                                                            batch_num_devices=1,
                                                            data_target_average_len=[None] * len(buckets))
 
@@ -682,7 +705,7 @@ def test_sharded_parallel_sample_iter_num_batches():
     num_batches = num_shards * num_batches_per_shard
     bucket_batch_sizes = data_io.define_bucket_batch_sizes(buckets,
                                                            batch_size,
-                                                           batch_by_words=False,
+                                                           batch_type=C.BATCH_TYPE_SENTENCE,
                                                            batch_num_devices=1,
                                                            data_target_average_len=[None] * len(buckets))
 
@@ -716,7 +739,7 @@ def test_sharded_and_parallel_iter_same_num_batches():
     num_batches = num_batches_per_bucket * len(buckets)
     bucket_batch_sizes = data_io.define_bucket_batch_sizes(buckets,
                                                            batch_size,
-                                                           batch_by_words=False,
+                                                           batch_type=C.BATCH_TYPE_SENTENCE,
                                                            batch_num_devices=1,
                                                            data_target_average_len=[None] * len(buckets))
 

commit 2cbad615d1f57ce3fd51063a998735eb6249ae91

Fix batch sizing errors introduced in c00da52 (#849)

sockeye/__init__.py
@@ -11,4 +11,4 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-__version__ = '2.1.15'
+__version__ = '2.1.16'

sockeye/data_io.py
@@ -212,13 +212,11 @@ def define_bucket_batch_sizes(buckets: List[Tuple[int, int]],
             # Max number of sequences without exceeding batch size
             batch_size_seq = batch_size // padded_seq_len
             # Round down to closest multiple
-            batch_size_seq = (batch_size_seq // batch_sentences_multiple_of) * batch_sentences_multiple_of
+            batch_size_seq = (batch_size_seq // min_batch_step) * min_batch_step
         elif batch_type == C.BATCH_TYPE_SENTENCE:
             batch_size_seq = batch_size
         else:
             raise ValueError('Unknown batch type: %s' % batch_type)
-        # Batch size is per device
-        batch_size_seq *= batch_num_devices
         # Number of words here is an average of non-padding tokens
         batch_size_word = batch_size_seq * average_seq_len
 

test/unit/test_data_io.py
@@ -228,17 +228,18 @@ def test_sample_based_define_bucket_batch_sizes():
         assert bbs.average_target_words_per_batch == bbs.bucket[1] * batch_size
 
 
-@pytest.mark.parametrize("length_ratio,batch_sentences_multiple_of,expected_batch_sizes", [
+@pytest.mark.parametrize("batch_num_devices,length_ratio,batch_sentences_multiple_of,expected_batch_sizes", [
         # Reference batch sizes manually inspected for sanity.  Note that for
         # very unbalanced lengths, the last batch can be very large.  This is
         # due to the requirement for any size batch (total elements) to fit into
         # the same allocated space for MXNet's memory sharing.
-        (0.5, 1, [200.0, 100.0, 67.0, 50.0, 40.0, 33.0, 29.0, 25.0, 22.0, 41.0]),
-        (1.5, 1, [100.0, 50.0, 33.0, 25.0, 20.0, 20.0, 20.0, 20.0]),
-        (1.5, 8, [96.0, 48.0, 32.0, 24.0, 16.0, 16.0, 16.0, 24.0])])
-def test_word_based_define_bucket_batch_sizes(length_ratio, batch_sentences_multiple_of, expected_batch_sizes):
+        (1, 0.5, 1, [200.0, 100.0, 67.0, 50.0, 40.0, 33.0, 29.0, 25.0, 22.0, 41.0]),
+        (2, 0.5, 1, [200.0, 100.0, 66.0, 50.0, 40.0, 34.0, 28.0, 24.0, 22.0, 40.0]),
+        (8, 0.5, 1, [200.0, 96.0, 64.0, 48.0, 40.0, 32.0, 32.0, 24.0, 24.0, 40.0]),
+        (1, 1.5, 1, [100.0, 50.0, 33.0, 25.0, 20.0, 20.0, 20.0, 20.0]),
+        (1, 1.5, 8, [96.0, 48.0, 32.0, 24.0, 16.0, 16.0, 16.0, 24.0])])
+def test_word_based_define_bucket_batch_sizes(batch_num_devices, length_ratio, batch_sentences_multiple_of, expected_batch_sizes):
     batch_type = C.BATCH_TYPE_WORD
-    batch_num_devices = 1
     batch_size = 1000
     max_seq_len = 50
     buckets = data_io.define_parallel_buckets(max_seq_len, max_seq_len, 10, True, length_ratio)
@@ -263,14 +264,15 @@ def test_word_based_define_bucket_batch_sizes(length_ratio, batch_sentences_mult
     assert last_bbs_num_words >= max_num_words
 
 
-@pytest.mark.parametrize("length_ratio,batch_sentences_multiple_of,expected_batch_sizes", [
+@pytest.mark.parametrize("batch_num_devices,length_ratio,batch_sentences_multiple_of,expected_batch_sizes", [
         # Reference batch sizes manually inspected for sanity.
-        (0.5, 1, [200, 100, 66, 50, 40, 33, 28, 25, 22, 20]),
-        (1.5, 1, [100, 50, 33, 25, 20, 20, 20, 20]),
-        (1.5, 8, [96, 48, 32, 24, 16, 16, 16, 16])])
-def test_max_word_based_define_bucket_batch_sizes(length_ratio, batch_sentences_multiple_of, expected_batch_sizes):
+        (1, 0.5, 1, [200, 100, 66, 50, 40, 33, 28, 25, 22, 20]),
+        (2, 0.5, 1, [200, 100, 66, 50, 40, 32, 28, 24, 22, 20]),
+        (8, 0.5, 1, [200, 96, 64, 48, 40, 32, 24, 24, 16, 16]),
+        (1, 1.5, 1, [100, 50, 33, 25, 20, 20, 20, 20]),
+        (1, 1.5, 8, [96, 48, 32, 24, 16, 16, 16, 16])])
+def test_max_word_based_define_bucket_batch_sizes(batch_num_devices, length_ratio, batch_sentences_multiple_of, expected_batch_sizes):
     batch_type = C.BATCH_TYPE_MAX_WORD
-    batch_num_devices = 1
     batch_size = 1000
     max_seq_len = 50
     buckets = data_io.define_parallel_buckets(max_seq_len, max_seq_len, 10, True, length_ratio)