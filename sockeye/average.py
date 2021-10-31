Port parameter averaging, fix summing tensors

sockeye/average.py
@@ -1,4 +1,4 @@
-# Copyright 2017--2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2017--2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You may not
 # use this file except in compliance with the License. A copy of the License
@@ -24,10 +24,10 @@ import os
 import logging
 from typing import Dict, Iterable, List
 
-import mxnet as mx
-from mxnet import np, npx
+import torch
+from mxnet import npx
 
-from sockeye.log import setup_main_logger, log_sockeye_version
+from .log import setup_main_logger, log_sockeye_version
 from . import arguments
 from . import constants as C
 from . import utils
@@ -35,17 +35,22 @@ from . import utils
 logger = logging.getLogger(__name__)
 
 
-def average(param_paths: Iterable[str]) -> Dict[str, np.ndarray]:
+def average(param_paths: Iterable[str]) -> Dict[str, torch.Tensor]:
     """
     Averages parameters from a list of .params file paths.
 
     :param param_paths: List of paths to parameter files.
     :return: Averaged parameter dictionary.
     """
-    all_params = []  # type: List[Dict[str, np.ndarray]]
+    all_params = []  # type: List[Dict[str, torch.Tensor]]
     for path in param_paths:
         logger.info("Loading parameters from '%s'", path)
-        params = npx.load(path)
+        try:
+            params = torch.load(path)
+        except:
+            logger.info('Converting from MXNet')
+            params = npx.load(path)
+            params = {k: torch.from_numpy(v.asnumpy()) for k, v in params.items()}
         all_params.append(params)
 
     logger.info("%d models loaded", len(all_params))
@@ -55,8 +60,8 @@ def average(param_paths: Iterable[str]) -> Dict[str, np.ndarray]:
     avg_params = {}
     # average arg_params
     for k in all_params[0]:
-        arrays = [p[k] for p in all_params]
-        avg_params[k] = utils.average_arrays(arrays)
+        tensors = [p[k] for p in all_params]
+        avg_params[k] = utils.average_tensors(tensors)
     return avg_params
 
 
@@ -160,7 +165,7 @@ def average_parameters(args: argparse.Namespace):
                                        metric=args.metric)
         avg_params = average(param_paths)
 
-    npx.save(args.output, avg_params)
+    torch.save(avg_params, args.output)
     logger.info("Averaged parameters written to '%s'", args.output)
 
 

sockeye/training_pt.py
@@ -317,7 +317,7 @@ class PyTorchEarlyStoppingTrainer:
             # of padding tokens). Consider normalizing by relative batch size.
             loss_values = [v / self.config.update_interval if self.config.update_interval > 1
                            else v for v, _ in loss_outputs]
-            sum_losses = torch.add(*loss_values) if len(loss_values) > 1 else loss_values[0]
+            sum_losses = sum(loss_values) if len(loss_values) > 1 else loss_values[0]
         if self.using_amp:
             sum_losses = self._scaler.scale(sum_losses)
         # Backward

test/unit/test_average.py
@@ -1,4 +1,4 @@
-# Copyright 2017--2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2017--2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You may not
 # use this file except in compliance with the License. A copy of the License
@@ -11,7 +11,12 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
+import os
+import tempfile
+
 import pytest
+
+import torch
 
 import sockeye.average as average
 
@@ -55,3 +60,23 @@ def test_strategy_last(test_points, expected_top_n, size, maximize):
 def test_strategy_lifespan(test_points, expected_top_n, size, maximize):
     result = average.strategy_lifespan(test_points, size, maximize)
     assert result == expected_top_n
+
+
+def test_average():
+    params1 = {'key1': torch.tensor([1., 1.]), 'key2': torch.tensor([[2., 2.], [2., 2.]])}
+    params2 = {'key1': torch.tensor([2., 2.]), 'key2': torch.tensor([[4., 4.], [4., 4.]])}
+    params_average = {'key1': torch.tensor([1.5, 1.5]), 'key2': torch.tensor([[3., 3.], [3., 3.]])}
+
+    with tempfile.TemporaryDirectory(prefix='test_average') as work_dir:
+        params1_fname = os.path.join(work_dir, 'params1')
+        params2_fname = os.path.join(work_dir, 'params2')
+        torch.save(params1, params1_fname)
+        torch.save(params2, params2_fname)
+
+        # "Average" one params file
+        for k, v in average.average([params1_fname]).items():
+            assert torch.allclose(v, params1[k])
+
+        # Average four params files (two instances each of two files)
+        for k, v in average.average([params1_fname, params1_fname, params2_fname, params2_fname]).items():
+            assert torch.allclose(v, params_average[k])