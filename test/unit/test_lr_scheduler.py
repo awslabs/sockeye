55f171179ac6ec2bc6d2cb0aec68c4875a24b562
Michael Denkowski <mdenkows@amazon.com>
2022-08-04 21:42:09 -0500

    Cleanup

--- test/unit/test_lr_scheduler.py
+++ test/unit/test_lr_scheduler.py
@@ -11,7 +11,6 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from sched import scheduler
 import pytest
 
 import numpy as np
